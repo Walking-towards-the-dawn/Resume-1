3aa8d0dc0719bdcff0e486182138a6b32c091cca
HryhorenkoValia <156314987+HryhorenkoValia@users.noreply.github.com>
2024-01-11 15:15:15 +0200

    Valia

---
+++ main.js
@@ -0,0 +1,9 @@
+function openModal() {
+	document.getElementById('overlay').style.display = 'flex';
+}
+
+function closeModal() {
+	document.getElementById('overlay').style.display = 'none';
+}
+
+/* ----- Animation ----- */
